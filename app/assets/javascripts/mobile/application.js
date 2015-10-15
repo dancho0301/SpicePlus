2bfe78c088d615529698da1af67b94c9789918d1
masked-rider-dancho <samuraimania.d@gmail.com>
2015-10-15 21:44:05 +0900

    test

--- app/assets/javascripts/mobile/application.js
+++ app/assets/javascripts/mobile/application.js
@@ -17,12 +17,12 @@
 
 $(function( ) {
   $(".article_image img").on("load",function(){
-    var iw, ih;
-    var cw = 206; /*トリミング後の横幅*/
-    var ch = 180; /*トリミング後の縦幅*/
-    iw = ($(this).width() - cw) / 2;
-    ih = ($(this).height() - ch) / 2;
-    $(this).css("top", "-"+ih+"px");
-    $(this).css("left", "-"+iw+"px");
+    // var iw, ih;
+    // var cw = 206; /*トリミング後の横幅*/
+    // var ch = 180; /*トリミング後の縦幅*/
+    // iw = ($(this).width() - cw) / 2;
+    // ih = ($(this).height() - ch) / 2;
+    // $(this).css("top", "-"+ih+"px");
+    // $(this).css("left", "-"+iw+"px");
   });
 });
