a7b258cbc35f3754adeec6b72752b4a5365d2715
masked-rider-dancho <samuraimania.d@gmail.com>
2016-03-05 14:31:10 +0900

    初期データは手動で登録することに。id:0は登録できないっぽいので

--- db/migrate/20160216114323_create_other_article.rb
+++ db/migrate/20160216114323_create_other_article.rb
@@ -2,11 +2,5 @@ class CreateOtherArticle < ActiveRecord::Migration
   def change
     rename_table :articles, :origin_articles
     add_column :origin_articles, :type, :string, default: "Article"
-
-    Genre.create do |g|
-      g.id = 0
-      g.name = 'お知らせ'
-      g.css_class = 'other'
-    end
   end
 end
