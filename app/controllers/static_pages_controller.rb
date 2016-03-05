e7a53337f12d3224f899549a982510da69f81bee
masked-rider-dancho <samuraimania.d@gmail.com>
2016-03-05 16:50:00 +0900

    モバイル対応が漏れていた

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
-  before_action :set_search
-  before_action :set_used_area
-  before_action :set_all_articles
+  # before_action :set_search
+  # before_action :set_used_area
+  # before_action :set_all_articles
 
   def about
   end
@@ -11,22 +11,21 @@ class StaticPagesController < ApplicationController
 
 
   private
-    def set_article
-      @article = Article.find(params[:id])
-    end
-
-
-    def set_search
-      @search = Article.search(params[:q])
-    end
+    # def set_article
+    #   @article = Article.find(params[:id])
+    # end
+    #
+    # def set_search
+    #   @search = Article.search(params[:q])
+    # end
 
     # 記事に利用されている地域だけ抽出する（検索で利用するため）
-    def set_used_area
-      @used_areas = Area.find_by_sql("select * from areas a where exists (select 1 from articles b where a.id = b.area_id)")
-    end
+    # def set_used_area
+    #   @used_areas = Area.find_by_sql("select * from areas a where exists (select 1 from origin_articles b where a.id = b.area_id)")
+    # end
 
-    def set_all_articles
-      @all_articles = Article.all
-    end
+    # def set_all_articles
+    #   @all_articles = Article.all
+    # end
 
 end
